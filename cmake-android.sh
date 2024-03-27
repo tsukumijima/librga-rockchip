f31b4dcf940fb4d051ac5e5708c1e1b37f8694be
Yu Qiaowei <cerf.yu@rock-chips.com>
2024-03-27 09:48:08 +0800

    build: cmake: sh: support switching compilation of C or C++
    Signed-off-by: Yu Qiaowei <cerf.yu@rock-chips.com>
    Change-Id: I977e7d99426266d0d93165e595711632027b9afe

--- cmake-android.sh
+++ cmake-android.sh
@@ -8,17 +8,27 @@ TOOLCHAIN_PATH=${SOURCE_PATH}/toolchains/toolchain_android_ndk.cmake
 BUILD_DIR=build/build_android_ndk
 BUILD_TYPE=Release
 
+if [ ${1} == 'c' ]
+then
+    echo "compile with C"
+	BUILD_SOURCE_TYPE=c
+else
+    echo "compile with C++"
+	BUILD_SOURCE_TYPE=cpp
+fi
+
 rm -rf $BUILD_DIR
 mkdir -p $BUILD_DIR
 pushd $BUILD_DIR
 
 cmake ../.. \
 	-DCMAKE_BUILD_TARGET=android_ndk \
+	-DRGA_SOURCE_CODE_TYPE=${BUILD_SOURCE_TYPE} \
 	-DBUILD_TOOLCHAINS_PATH=${TOOLCHAIN_PATH} \
 	-DCMAKE_BUILD_TYPE=${BUILD_TYPE} \
 	-DCMAKE_INSTALL_PREFIX=install \
 
-make -j8
+make -j32
 make install
 
 popd
--- cmake-linux.sh
+++ cmake-linux.sh
@@ -8,17 +8,27 @@ TOOLCHAIN_PATH=${SOURCE_PATH}/toolchains/toolchain_linux.cmake
 BUILD_DIR=build/build_linux
 BUILD_TYPE=Release
 
+if [ ${1} == 'c' ]
+then
+    echo "compile with C"
+	BUILD_SOURCE_TYPE=c
+else
+    echo "compile with C++"
+	BUILD_SOURCE_TYPE=cpp
+fi
+
 rm -rf $BUILD_DIR
 mkdir -p $BUILD_DIR
 pushd $BUILD_DIR
 
 cmake ../.. \
 	-DCMAKE_BUILD_TARGET=buildroot \
+	-DRGA_SOURCE_CODE_TYPE=${BUILD_SOURCE_TYPE} \
 	-DBUILD_TOOLCHAINS_PATH=${TOOLCHAIN_PATH} \
 	-DCMAKE_BUILD_TYPE=${BUILD_TYPE} \
 	-DCMAKE_INSTALL_PREFIX=install \
 
-make -j8
+make -j32
 make install
 
 popd
