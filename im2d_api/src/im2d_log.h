cee109a80a79432f245c3b77efce5160311c84c7
Yu Qiaowei <cerf.yu@rock-chips.com>
2024-05-07 09:57:01 +0800

    im2d_api: log: modify the prefix of IM_LOG in Linux/AndroidNDK
    Signed-off-by: Yu Qiaowei <cerf.yu@rock-chips.com>
    Change-Id: I0e1ebc4aeff63156b8769e8c9a45290903e67935

--- im2d_api/src/im2d_log.h
+++ im2d_api/src/im2d_log.h
@@ -18,6 +18,9 @@
 #ifndef _im2d_log_hpp_
 #define _im2d_log_hpp_
 
+#include <unistd.h>
+#include <sys/syscall.h>
+
 #define IM_ERR_MSG_LEN 512
 
 typedef enum {
@@ -34,6 +37,8 @@ typedef enum {
 
 #define GET_LOG_LEVEL(level) ((level) & IM_LOG_LEVEL_MASK)
 #define LOG_LEVEL_CHECK(level) ((level) >= rga_log_level_get())
+
+const char *rga_get_error_type_str(int type);
 
 int rga_error_msg_set(const char* format, ...);
 int rga_log_level_init(void);
@@ -71,9 +76,10 @@ size_t rga_get_start_time_ms(void);
         if ((rga_log_enable_get() > 0 && LOG_LEVEL_CHECK(level)) || \
             GET_LOG_LEVEL(level) == IM_LOG_ERROR || \
             (level) & IM_LOG_FORCE) \
-            fprintf(stdout, "%lu " LOG_TAG " %s(%d): " _str "\n", \
+            fprintf(stdout, "%lu %6lu %6d %1s %8s: " _str "\n", \
                     (unsigned long)(rga_get_current_time_ms()-rga_get_start_time_ms()), \
-                    __FUNCTION__, __LINE__, ## __VA_ARGS__); \
+                    syscall(SYS_gettid), getpid(), rga_get_error_type_str(level), LOG_TAG, \
+                    ## __VA_ARGS__); \
     } while(0)
 #define IM_LOGD(_str, ...) IM_LOG(IM_LOG_DEBUG, _str , ## __VA_ARGS__)
 #define IM_LOGI(_str, ...) IM_LOG(IM_LOG_INFO, _str , ## __VA_ARGS__)
