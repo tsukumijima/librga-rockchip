tsukumijima/librga-rockchip

commit cc620bc3d6f3349155ef14429340aac65917acdd

im2d_api: support blend between formats without per-pixel alpha
Signed-off-by: Yu Qiaowei <cerf.yu@rock-chips.com>
Change-Id: Ic93bab7619d05ac2ef0d9a8955b73a8add809d0c

im2d_api/src/im2d_impl.cpp
@@ -1106,52 +1106,15 @@ IM_STATUS rga_check_blend(rga_buffer_t src, rga_buffer_t pat, rga_buffer_t dst, 
     pat_isRGB = NormalRgaIsRgbFormat(pat_fmt);
     dst_isRGB = NormalRgaIsRgbFormat(dst_fmt);
 
-    /**************** blend mode check ****************/
-    switch (mode_usage & IM_ALPHA_BLEND_MASK) {
-        case IM_ALPHA_BLEND_SRC :
-        case IM_ALPHA_BLEND_DST :
-            break;
-        case IM_ALPHA_BLEND_SRC_OVER :
-            if (!NormalRgaFormatHasAlpha(src_fmt)) {
-                IM_LOGW("Blend mode 'src_over' unsupported src format without alpha, "
-                        "format[src,src1,dst] = [0x%x(%s), 0x%x(%s), 0x%x(%s)]",
-                        src_fmt, translate_format_str(src_fmt),
-                        pat_fmt, translate_format_str(pat_fmt),
-                        dst_fmt, translate_format_str(dst_fmt));
-                return IM_STATUS_NOT_SUPPORTED;
-            }
-            break;
-        case IM_ALPHA_BLEND_DST_OVER :
-            if (pat_enable) {
-                if (!NormalRgaFormatHasAlpha(pat_fmt)) {
-                    IM_LOGW("Blend mode 'dst_over' unsupported pat format without alpha, "
-                            "format[src,src1,dst] = [0x%x(%s), 0x%x(%s), 0x%x(%s)]",
-                            src_fmt, translate_format_str(src_fmt),
-                            pat_fmt, translate_format_str(pat_fmt),
-                            dst_fmt, translate_format_str(dst_fmt));
-                    return IM_STATUS_NOT_SUPPORTED;
-                }
-            } else {
-                if (!NormalRgaFormatHasAlpha(dst_fmt)) {
-                    IM_LOGW("Blend mode 'dst_over' unsupported dst format without alpha, "
-                            "format[src,src1,dst] = [0x%x(%s), 0x%x(%s), 0x%x(%s)]",
-                            src_fmt, translate_format_str(src_fmt),
-                            pat_fmt, translate_format_str(pat_fmt),
-                            dst_fmt, translate_format_str(dst_fmt));
-                    return IM_STATUS_NOT_SUPPORTED;
-                }
-            }
-            break;
-        default :
-            if (!(NormalRgaFormatHasAlpha(src_fmt) || NormalRgaFormatHasAlpha(dst_fmt))) {
-                IM_LOGW("Blend mode unsupported format without alpha, "
-                        "format[src,src1,dst] = [0x%x(%s), 0x%x(%s), 0x%x(%s)]",
-                        src_fmt, translate_format_str(src_fmt),
-                        pat_fmt, translate_format_str(pat_fmt),
-                        dst_fmt, translate_format_str(dst_fmt));
-                return IM_STATUS_NOT_SUPPORTED;
-            }
-            break;
+    /* bg format check */
+    if (rga_is_buffer_valid(pat) && !pat_isRGB) {
+        IM_LOGW("Blend mode background layer unsupport non-RGB format, pat format = %#x(%s)",
+            pat_fmt, translate_format_str(pat_fmt));
+        return IM_STATUS_NOT_SUPPORTED;
+    } else if (!dst_isRGB) {
+        IM_LOGW("Blend mode background layer unsupport non-RGB format, dst format = %#x(%s)",
+            dst_fmt, translate_format_str(dst_fmt));
+        return IM_STATUS_NOT_SUPPORTED;
     }
 
     /* src1 don't support scale, and src1's size must aqual to dst.' */

commit fc3f742a7be43c62c9eb3e17a007f957b0e7db83

im2d_api: fix wrong check dst-channel format in 3-channel mode
Fixes: cc620bc3d6f3 (im2d_api: support blend between formats without per-pixel alpha)

update to 1.10.1_[6]

Signed-off-by: Yu Qiaowei <cerf.yu@rock-chips.com>
Change-Id: Ia01c290e92d1aee3fd7c1033d512771647fec7a6

im2d_api/im2d_version.h
@@ -26,7 +26,7 @@
 #define RGA_API_MAJOR_VERSION       1
 #define RGA_API_MINOR_VERSION       10
 #define RGA_API_REVISION_VERSION    1
-#define RGA_API_BUILD_VERSION       4
+#define RGA_API_BUILD_VERSION       6
 
 #define RGA_API_SUFFIX
 

im2d_api/src/im2d_impl.cpp
@@ -1107,14 +1107,18 @@ IM_STATUS rga_check_blend(rga_buffer_t src, rga_buffer_t pat, rga_buffer_t dst, 
     dst_isRGB = NormalRgaIsRgbFormat(dst_fmt);
 
     /* bg format check */
-    if (rga_is_buffer_valid(pat) && !pat_isRGB) {
-        IM_LOGW("Blend mode background layer unsupport non-RGB format, pat format = %#x(%s)",
-            pat_fmt, translate_format_str(pat_fmt));
-        return IM_STATUS_NOT_SUPPORTED;
-    } else if (!dst_isRGB) {
-        IM_LOGW("Blend mode background layer unsupport non-RGB format, dst format = %#x(%s)",
-            dst_fmt, translate_format_str(dst_fmt));
-        return IM_STATUS_NOT_SUPPORTED;
+    if (rga_is_buffer_valid(pat)) {
+        if (!pat_isRGB) {
+            IM_LOGW("Blend mode background layer unsupport non-RGB format, pat format = %#x(%s)",
+                pat_fmt, translate_format_str(pat_fmt));
+            return IM_STATUS_NOT_SUPPORTED;
+        }
+    } else {
+        if (!dst_isRGB) {
+            IM_LOGW("Blend mode background layer unsupport non-RGB format, dst format = %#x(%s)",
+                dst_fmt, translate_format_str(dst_fmt));
+            return IM_STATUS_NOT_SUPPORTED;
+        }
     }
 
     /* src1 don't support scale, and src1's size must aqual to dst.' */