JeffyCN/mirrors

commit cb1902fc5b480adcf82cc0d561d7f43da92c26e1

add description of RK_FORMAT_XX
Change-Id: If4c7ed9ae8c477a17b8f1a922e610d63e6ffce33
Signed-off-by: Yu Qiaowei <cerf.yu@rock-chips.com>

include/rga.h
@@ -36,66 +36,106 @@ extern "C"
 /* In order to be compatible with RK_FORMAT_XX and HAL_PIXEL_FORMAT_XX,
  * RK_FORMAT_XX is shifted to the left by 8 bits to distinguish.  */
 typedef enum _Rga_SURF_FORMAT {
-    RK_FORMAT_RGBA_8888    = 0x0 << 8,
-    RK_FORMAT_RGBX_8888    = 0x1 << 8,
-    RK_FORMAT_RGB_888      = 0x2 << 8,
-    RK_FORMAT_BGRA_8888    = 0x3 << 8,
-    RK_FORMAT_RGB_565      = 0x4 << 8,
-    RK_FORMAT_RGBA_5551    = 0x5 << 8,
-    RK_FORMAT_RGBA_4444    = 0x6 << 8,
-    RK_FORMAT_BGR_888      = 0x7 << 8,
+    RK_FORMAT_RGBA_8888    = 0x0 << 8,  /* [0:31] R:G:B:A 8:8:8:8 little endian */
+    RK_FORMAT_RGBX_8888    = 0x1 << 8,  /* [0:31] R:G:B:X 8:8:8:8 little endian */
+    RK_FORMAT_RGB_888      = 0x2 << 8,  /* [0:23] R:G:B 8:8:8 little endian */
+    RK_FORMAT_BGRA_8888    = 0x3 << 8,  /* [0:31] B:G:R:A 8:8:8:8 little endian */
+    RK_FORMAT_RGB_565      = 0x4 << 8,  /* [0:15] R:G:B 5:6:5 little endian */
+    RK_FORMAT_RGBA_5551    = 0x5 << 8,  /* [0:15] R:G:B:A 5:5:5:1 little endian */
+    RK_FORMAT_RGBA_4444    = 0x6 << 8,  /* [0:15] R:G:B:A 4:4:4:4 little endian */
+    RK_FORMAT_BGR_888      = 0x7 << 8,  /* [0:23] B:G:R 8:8:8 little endian */
 
-    RK_FORMAT_YCbCr_422_SP = 0x8 << 8,
-    RK_FORMAT_YCbCr_422_P  = 0x9 << 8,
-    RK_FORMAT_YCbCr_420_SP = 0xa << 8,
-    RK_FORMAT_YCbCr_420_P  = 0xb << 8,
+    RK_FORMAT_YCbCr_422_SP = 0x8 << 8,  /* 2 plane YCbCr little endian
+                                         * plane 0: [0:7] Y
+                                         * plane 1: 2x1 subsampled [0:15] Cb:Cr 8:8 */
+    RK_FORMAT_YCbCr_422_P  = 0x9 << 8,  /* 3 plane YCbCr little endian
+                                         * plane 0: [0:7] Y
+                                         * plane 1: 2x1 subsampled [0:7] Cb
+                                         * plane 2: 2x1 subsampled [0:7] Cr */
+    RK_FORMAT_YCbCr_420_SP = 0xa << 8,  /* 2 plane YCbCr little endian
+                                         * plane 0: [0:7] Y
+                                         * plane 1: 2x2 subsampled [0:15] Cr:Cb 8:8 */
+    RK_FORMAT_YCbCr_420_P  = 0xb << 8,  /* 3 plane YCbCr little endian
+                                         * plane 0: [0:7] Y
+                                         * plane 1: 2x2 subsampled [0:7] Cb
+                                         * plane 2: 2x2 subsampled [0:7] Cr */
 
-    RK_FORMAT_YCrCb_422_SP = 0xc << 8,
-    RK_FORMAT_YCrCb_422_P  = 0xd << 8,
-    RK_FORMAT_YCrCb_420_SP = 0xe << 8,
-    RK_FORMAT_YCrCb_420_P  = 0xf << 8,
+    RK_FORMAT_YCrCb_422_SP = 0xc << 8,  /* 2 plane YCbCr little endian
+                                         * plane 0: [0:7] Y
+                                         * plane 1: 2x1 subsampled [0:15] Cb:Cr 8:8 */
+    RK_FORMAT_YCrCb_422_P  = 0xd << 8,  /* 3 plane YCbCr little endian
+                                         * plane 0: [0:7] Y
+                                         * plane 1: 2x1 subsampled [0:7] Cr
+                                         * plane 2: 2x1 subsampled [0:7] Cb */
+    RK_FORMAT_YCrCb_420_SP = 0xe << 8,  /* 2 plane YCbCr little endian
+                                         * plane 0: [0:7] Y
+                                         * plane 1: 2x2 subsampled [0:15] Cb:Cr 8:8 */
+    RK_FORMAT_YCrCb_420_P  = 0xf << 8,  /* 3 plane YCbCr little endian
+                                         * plane 0: [0:7] Y
+                                         * plane 1: 2x2 subsampled [0:7] Cr
+                                         * plane 2: 2x2 subsampled [0:7] Cb */
 
-    RK_FORMAT_BPP1         = 0x10 << 8,
-    RK_FORMAT_BPP2         = 0x11 << 8,
-    RK_FORMAT_BPP4         = 0x12 << 8,
-    RK_FORMAT_BPP8         = 0x13 << 8,
+    RK_FORMAT_BPP1         = 0x10 << 8, /* [0] little endian */
+    RK_FORMAT_BPP2         = 0x11 << 8, /* [0:1] little endian */
+    RK_FORMAT_BPP4         = 0x12 << 8, /* [0:3] little endian */
+    RK_FORMAT_BPP8         = 0x13 << 8, /* [0:7] little endian */
 
-    RK_FORMAT_Y4           = 0x14 << 8,
-    RK_FORMAT_YCbCr_400    = 0x15 << 8,
+    RK_FORMAT_Y4           = 0x14 << 8, /* [0:3] Y little endian */
+    RK_FORMAT_YCbCr_400    = 0x15 << 8, /* [0:7] Y little endian */
 
-    RK_FORMAT_BGRX_8888    = 0x16 << 8,
+    RK_FORMAT_BGRX_8888    = 0x16 << 8, /* [0:31] B:G:R:X 8:8:8:8 little endian */
 
-    RK_FORMAT_YVYU_422     = 0x18 << 8,
-    RK_FORMAT_YVYU_420     = 0x19 << 8,
-    RK_FORMAT_VYUY_422     = 0x1a << 8,
-    RK_FORMAT_VYUY_420     = 0x1b << 8,
-    RK_FORMAT_YUYV_422     = 0x1c << 8,
-    RK_FORMAT_YUYV_420     = 0x1d << 8,
-    RK_FORMAT_UYVY_422     = 0x1e << 8,
-    RK_FORMAT_UYVY_420     = 0x1f << 8,
+    RK_FORMAT_YVYU_422     = 0x18 << 8, /* [0:31] Y0:Cr0:Y1:cb0 8:8:8:8 little endian */
+    RK_FORMAT_YVYU_420     = 0x19 << 8, /* ODD : [0:31] Y0:Cr0:Y1:cb0 8:8:8:8 little endian
+                                         * EVEN: [0:31] Y2:Y3:X:X 8:8:8:8 little endian */
+    RK_FORMAT_VYUY_422     = 0x1a << 8, /* [0:31] Cr0:Y0:Cb0:Y1 8:8:8:8 little endian */
+    RK_FORMAT_VYUY_420     = 0x1b << 8, /* ODD : [0:31] Cr0:Y0:Cb0:Y1 8:8:8:8 little endian
+                                         * EVEN: [0:31] Y2:Y3:X:X 8:8:8:8 little endian */
+    RK_FORMAT_YUYV_422     = 0x1c << 8, /* [0:31] Y0:Cb0:Y1:cr0 8:8:8:8 little endian */
+    RK_FORMAT_YUYV_420     = 0x1d << 8, /* ODD : [0:31] Y0:Cb0:Y1:cr0 8:8:8:8 little endian
+                                         * EVEN: [0:31] Y2:Y3:X:X 8:8:8:8 little endian */
+    RK_FORMAT_UYVY_422     = 0x1e << 8, /* [0:31] Cb0:Y0:Cr0:Y1 8:8:8:8 little endian */
+    RK_FORMAT_UYVY_420     = 0x1f << 8, /* ODD : [0:31] Cb0:Y0:Cr0:Y1 8:8:8:8 little endian
+                                         * EVEN: [0:31] Y2:Y3:X:X 8:8:8:8 little endian */
 
-    RK_FORMAT_YCbCr_420_SP_10B = 0x20 << 8,
-    RK_FORMAT_YCrCb_420_SP_10B = 0x21 << 8,
-    RK_FORMAT_YCbCr_422_SP_10B = 0x22 << 8,
-    RK_FORMAT_YCrCb_422_SP_10B = 0x23 << 8,
+    RK_FORMAT_YCbCr_420_SP_10B = 0x20 << 8, /* 2 plane YCbCr little endian
+                                             * plane 0: [0:9] Y
+                                             * plane 1: 2x2 subsampled [0:19] Cb:Cr 10: 10 (default)
+                                             * or
+                                             * plane 1: 2x2 subsampled [0:23] Cb:Cr 16: 16 */
+    RK_FORMAT_YCrCb_420_SP_10B = 0x21 << 8, /* 2 plane YCbCr little endian
+                                             * plane 0: [0:9] Y
+                                             * plane 1: 2x2 subsampled [0:19] Cr:Cb 10: 10 (default)
+                                             * or
+                                             * plane 1: 2x2 subsampled [0:23] Cr:Cb 16: 16 */
+    RK_FORMAT_YCbCr_422_SP_10B = 0x22 << 8, /* 2 plane YCbCr little endian
+                                             * plane 0: [0:9] Y
+                                             * plane 1: 2x1 subsampled [0:19] Cb:Cr 10:10  (default)
+                                             * or
+                                             * plane 1: 2x1 subsampled [0:23] Cb:Cr 16: 16 */
+    RK_FORMAT_YCrCb_422_SP_10B = 0x23 << 8, /* 2 plane YCbCr little endian
+                                             * plane 0: [0:9] Y
+                                             * plane 1: 2x1 subsampled [0:19] Cr:Cb 10:10  (default)
+                                             * or
+                                             * plane 1: 2x1 subsampled [0:23] Cr:Cb 16: 16 */
     /* For compatibility with misspellings */
-    RK_FORMAT_YCbCr_422_10b_SP = RK_FORMAT_YCbCr_422_SP_10B,
-    RK_FORMAT_YCrCb_422_10b_SP = RK_FORMAT_YCrCb_422_SP_10B,
+    RK_FORMAT_YCbCr_422_10b_SP = RK_FORMAT_YCbCr_422_SP_10B << 8,
+    RK_FORMAT_YCrCb_422_10b_SP = RK_FORMAT_YCrCb_422_SP_10B << 8,
 
-    RK_FORMAT_BGR_565      = 0x24 << 8,
-    RK_FORMAT_BGRA_5551    = 0x25 << 8,
-    RK_FORMAT_BGRA_4444    = 0x26 << 8,
+    RK_FORMAT_BGR_565      = 0x24 << 8, /* [0:16] B:G:R 5:6:5 little endian */
+    RK_FORMAT_BGRA_5551    = 0x25 << 8, /* [0:16] B:G:R:A 5:5:5:1 little endian */
+    RK_FORMAT_BGRA_4444    = 0x26 << 8, /* [0:16] B:G:R:A 4:4:4:4 little endian */
 
-    RK_FORMAT_ARGB_8888    = 0x28 << 8,
-    RK_FORMAT_XRGB_8888    = 0x29 << 8,
-    RK_FORMAT_ARGB_5551    = 0x2a << 8,
-    RK_FORMAT_ARGB_4444    = 0x2b << 8,
-    RK_FORMAT_ABGR_8888    = 0x2c << 8,
-    RK_FORMAT_XBGR_8888    = 0x2d << 8,
-    RK_FORMAT_ABGR_5551    = 0x2e << 8,
-    RK_FORMAT_ABGR_4444    = 0x2f << 8,
+    RK_FORMAT_ARGB_8888    = 0x28 << 8, /* [0:31] A:R:G:B 8:8:8:8 little endian */
+    RK_FORMAT_XRGB_8888    = 0x29 << 8, /* [0:31] X:R:G:B 8:8:8:8 little endian */
+    RK_FORMAT_ARGB_5551    = 0x2a << 8, /* [0:16] A:R:G:B 5:5:5:1 little endian */
+    RK_FORMAT_ARGB_4444    = 0x2b << 8, /* [0:16] A:R:G:B 4:4:4:4 little endian */
+    RK_FORMAT_ABGR_8888    = 0x2c << 8, /* [0:31] A:B:G:R 8:8:8:8 little endian */
+    RK_FORMAT_XBGR_8888    = 0x2d << 8, /* [0:31] X:B:G:R 8:8:8:8 little endian */
+    RK_FORMAT_ABGR_5551    = 0x2e << 8, /* [0:16] A:B:G:R 5:5:5:1 little endian */
+    RK_FORMAT_ABGR_4444    = 0x2f << 8, /* [0:16] A:B:G:R 4:4:4:4 little endian */
 
-    RK_FORMAT_RGBA2BPP     = 0x30 << 8,
+    RK_FORMAT_RGBA2BPP     = 0x30 << 8, /* [0:1] Color:Alpha 1:1 little endian */
 
     RK_FORMAT_UNKNOWN      = 0x100 << 8,
 } RgaSURF_FORMAT;

tsukumijima/librga-rockchip

commit 747c896d333b1bf6bf4d7e8c2bc1aabd510cd64f

im2d_api: fix incorrect definition of YUV_422_semi_planar format
Fixes: cb1902fc5b48 (add description of RK_FORMAT_XX)

update to 1.10.1_[7]

Signed-off-by: Yu Qiaowei <cerf.yu@rock-chips.com>
Change-Id: Idd8d1bd1294ef1ff5c782b14d905aeb40547a666

core/NormalRgaApi.cpp
@@ -543,8 +543,8 @@ bool NormalRgaIsYuvFormat(int format) {
         case RK_FORMAT_YCbCr_400:
         case RK_FORMAT_YCbCr_420_SP_10B:
         case RK_FORMAT_YCrCb_420_SP_10B:
-        case RK_FORMAT_YCrCb_422_10b_SP:
-        case RK_FORMAT_YCbCr_422_10b_SP:
+        case RK_FORMAT_YCbCr_422_SP_10B:
+        case RK_FORMAT_YCrCb_422_SP_10B:
         case RK_FORMAT_YCbCr_444_SP:
         case RK_FORMAT_YCrCb_444_SP:
         case RK_FORMAT_Y8:

core/RgaUtils.cpp
@@ -86,8 +86,8 @@ const struct format_table_entry format_table[] = {
 
     { RK_FORMAT_YCbCr_420_SP_10B,   "nv12_10" },
     { RK_FORMAT_YCrCb_420_SP_10B,   "crcb420sp_10" },
-    { RK_FORMAT_YCbCr_422_10b_SP,   "cbcr422_10b" },
-    { RK_FORMAT_YCrCb_422_10b_SP,   "crcb422_10b" },
+    { RK_FORMAT_YCbCr_422_SP_10B,   "cbcr422_10b" },
+    { RK_FORMAT_YCrCb_422_SP_10B,   "crcb422_10b" },
 
     { RK_FORMAT_BGR_565,            "bgr565" },
     { RK_FORMAT_BGRA_5551,          "bgra5551" },
@@ -224,8 +224,8 @@ float get_bpp_from_format(int format) {
         case RK_FORMAT_YCrCb_420_SP_10B:
             bpp = 2;
             break;
-        case RK_FORMAT_YCbCr_422_10b_SP:
-        case RK_FORMAT_YCrCb_422_10b_SP:
+        case RK_FORMAT_YCbCr_422_SP_10B:
+        case RK_FORMAT_YCrCb_422_SP_10B:
             bpp = 2.5;
             break;
         case RK_FORMAT_BGR_888:
@@ -299,8 +299,8 @@ int get_perPixel_stride_from_format(int format) {
             return  (1 * 8);
         case RK_FORMAT_YCbCr_420_SP_10B:
         case RK_FORMAT_YCrCb_420_SP_10B:
-        case RK_FORMAT_YCbCr_422_10b_SP:
-        case RK_FORMAT_YCrCb_422_10b_SP:
+        case RK_FORMAT_YCbCr_422_SP_10B:
+        case RK_FORMAT_YCrCb_422_SP_10B:
             return  (1 * 10);
         case RK_FORMAT_RGB_565:
         case RK_FORMAT_RGBA_5551:

im2d_api/im2d_version.h
@@ -26,7 +26,7 @@
 #define RGA_API_MAJOR_VERSION       1
 #define RGA_API_MINOR_VERSION       10
 #define RGA_API_REVISION_VERSION    1
-#define RGA_API_BUILD_VERSION       6
+#define RGA_API_BUILD_VERSION       7
 
 #define RGA_API_SUFFIX
 

im2d_api/src/im2d_impl.cpp
@@ -924,7 +924,7 @@ IM_STATUS rga_check_format(const char *name, rga_buffer_t info, im_rect rect, in
         if (ret != IM_STATUS_SUCCESS)
             return ret;
         IM_LOGW("If it is an RK encoder output, it needs to be aligned with an odd multiple of 256.\n");
-    } else if (format == RK_FORMAT_YCrCb_422_10b_SP || format == RK_FORMAT_YCbCr_422_10b_SP) {
+    } else if (format == RK_FORMAT_YCrCb_422_SP_10B || format == RK_FORMAT_YCbCr_422_SP_10B) {
         if (~format_usage & IM_RGA_SUPPORT_FORMAT_YUV_422_SEMI_PLANNER_10_BIT) {
             IM_LOGW("%s unsupported YUV422 semi-planner 10bit format, format = 0x%x(%s)\n%s",
                     name, info.format, translate_format_str(info.format),

include/rga.h
@@ -110,8 +110,8 @@ typedef enum _Rga_SURF_FORMAT {
                                              * or
                                              * plane 1: 2x1 subsampled [0:23] Cr:Cb 16: 16 */
     /* For compatibility with misspellings */
-    RK_FORMAT_YCbCr_422_10b_SP = RK_FORMAT_YCbCr_422_SP_10B << 8,
-    RK_FORMAT_YCrCb_422_10b_SP = RK_FORMAT_YCrCb_422_SP_10B << 8,
+    RK_FORMAT_YCbCr_422_10b_SP = RK_FORMAT_YCbCr_422_SP_10B,
+    RK_FORMAT_YCrCb_422_10b_SP = RK_FORMAT_YCrCb_422_SP_10B,
 
     RK_FORMAT_BGR_565      = 0x24 << 8, /* [0:16] B:G:R 5:6:5 little endian */
     RK_FORMAT_BGRA_5551    = 0x25 << 8, /* [0:16] B:G:R:A 5:5:5:1 little endian */